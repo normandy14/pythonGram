75dde33ac98a78c77d7fa810d26f21ce3593ced0
normandy14 <sturdyprogrammer@gmail.com>
2024-12-02 10:36:14 -0500

    Refactor main function. Refactor removeCharAtEnd function

--- app.py
+++ app.py
@@ -4,17 +4,22 @@
 
 # remove commas and periods and odd punctuation
 
-DEBUG = True
-DEBUG2 = True
+DEBUG = False
+DEBUG2 = False
+DEBUG3 = True
+DEBUG4 = False
+
+# Future Bug:  if its way-- and check if [-1] is -, then check word [-2] for -, and then remove
 
 def removeCharAtEnd(word):
-    print (word[-1])
-    print (word[-1].isalpha())
-    # if (word[-1].isalpha() != )
+    if (DEBUG4): print (word[-1])
+    if (DEBUG4): print (word[-1].isalpha())
+    if (word[-1].isalpha() != True):
+        word = word[:-1]
+    if (DEBUG4): print (word)
+    return word
 
 def main():
-    print ("Hello World")
-
     text = ""
     with open("taleSmall.txt") as file:
         text = file.read()
@@ -22,14 +27,15 @@ def main():
     arr = text.split()
     arrString = []
     for word in arr:
-        print (word)
-        word2 = word.strip()
-        print (word2)
+        # word = word.strip() # an extra precaution
+        if (DEBUG3): print ("word before is: {}".format(word))
+        newWord = removeCharAtEnd(word)
+        if (DEBUG3): print ("word after is: {}".format(newWord))
         arrString.append(word)
 
     # if (DEBUG): print (arr)
     if (DEBUG2): print (arrString)
 
 if __name__ == "__main__":
-    # main()
-    removeCharAtEnd("Hello")
+    main()
+    # removeCharAtEnd("Hello!")
