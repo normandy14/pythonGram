31f7f37be1be374c39a81a62c3a6e62ffe45696e
normandy14 <sturdyprogrammer@gmail.com>
2024-12-02 13:37:25 -0500

    Create seperateWordDash function and makeLowerCase function

--- app.py
+++ app.py
@@ -3,13 +3,28 @@
 # Seperate using space
 
 # remove commas and periods and odd punctuation
+# look out for special -- condition
+# an even more special case: way--in
 
 DEBUG = False
 DEBUG2 = False
-DEBUG3 = True
+DEBUG3 = False
 DEBUG4 = False
+DEBUG5 = True
+DEBUG6 = True
 
 # Future Bug:  if its way-- and check if [-1] is -, then check word [-2] for -, and then remove
+
+def seperateDashWords(word):
+    i = 0
+    for c in word:
+        if c == '-':
+            if (DEBUG6) : print ("Word 1 is: {}".format(word[:i]))
+            if (DEBUG6) : print ("Word 2 is: {}".format(word[i+2:]))
+            word1 = word[:i]
+            word2 = word[i+2:]
+            return [word1, word2]
+        i += 1
 
 def removeCharAtEnd(word):
     if (DEBUG4): print (word[-1])
@@ -17,6 +32,10 @@ def removeCharAtEnd(word):
     if (word[-1].isalpha() != True):
         word = word[:-1]
     if (DEBUG4): print (word)
+    return word
+
+def makeLowerCase(word):
+    word = word.lower()
     return word
 
 def main():
@@ -27,14 +46,25 @@ def main():
     arr = text.split()
     arrString = []
     for word in arr:
-        # word = word.strip() # an extra precaution
-        if (DEBUG3): print ("word before is: {}".format(word))
+        if (DEBUG3): print ("word before transformation is: {}".format(word))
         newWord = removeCharAtEnd(word)
-        if (DEBUG3): print ("word after is: {}".format(newWord))
-        arrString.append(word)
-
-    # if (DEBUG): print (arr)
-    if (DEBUG2): print (arrString)
+        if (DEBUG3): print ("word after remove at end is: {}".format(newWord))
+        newWord2 = makeLowerCase(word)
+        # if (DEBUG4): print ("word after lowercase is: {}".format(newWord2))
+        arrString.append(newWord2)
+    if (DEBUG5): print (arrString)
+    '''
+    arrString2 = []
+    for word in arrString:
+        if "--" in word:
+            if (DEBUG6) : print ("Dash in Word: {}".format(word))
+            words =  seperateDashWords(word)
+            arrString2.append(words[0])
+            arrString2.append(words[1])
+        else:
+            arrString2.append(word)
+    if (DEBUG6) : print (arrString2)
+    '''
 
 if __name__ == "__main__":
     main()
---
+++ test.py
@@ -0,0 +1,34 @@
+
+
+def getDashOut(word):
+    if "-" in word:
+        print ("dash in word")
+    else:
+        print ("no dash in word")
+
+def getDashOut2(word):
+    if "--" in word:
+        print ("Dash in Word: {}".format(word))
+        i = 0
+        for c in word:
+            if c == '-':
+                print ("Word 1 is: {}".format(word[:i]))
+                print ("Word 2 is: {}".format(word[i+2:]))
+                word1 = word[:i]
+                word2 = word[i+2:]
+                return [word1, word2]
+            i += 1
+
+    else:
+        print ("no dash in word")
+
+if __name__ == "__main__":
+    words = getDashOut2("cat--person")
+    print (words)
+    """
+    getDashOut2("dog")
+    getDashOut2("cat--person")
+    getDashOut2("television")
+    getDashOut2("make-shift")
+    getDashOut2("hat")
+    """
